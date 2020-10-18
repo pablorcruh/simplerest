5311f712dc473ec80b44d4124ff9da3bb256e8b3
pablorcruh <pablorcruh@gmail.com>
2020-10-18 16:01:36 -0500

    add project setup

---
+++ src/main/java/ec/com/pablorcruh/simplerest/app/SimplerestApplication.java
@@ -0,0 +1,13 @@
+package ec.com.pablorcruh.simplerest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplerestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplerestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/com/pablorcruh/simplerest/app/SimplerestApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.pablorcruh.simplerest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplerestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
