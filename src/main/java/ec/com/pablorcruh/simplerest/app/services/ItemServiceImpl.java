caee3d780a29bc1e981d318a820f204738283572
pablorcruh <pablorcruh@gmail.com>
2020-10-18 16:37:57 -0500

    add service implementation

---
+++ src/main/java/ec/com/pablorcruh/simplerest/app/services/ItemServiceImpl.java
@@ -0,0 +1,43 @@
+package ec.com.pablorcruh.simplerest.app.services;
+
+import ec.com.pablorcruh.simplerest.app.models.Item;
+import ec.com.pablorcruh.simplerest.app.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Pablo Cruz
+ */
+
+@Service
+public class ItemServiceImpl implements ItemService{
+
+    private ItemRepository repository;
+
+    @Autowired
+    public ItemServiceImpl(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Item> getAllItem() {
+        return (List<Item>) repository.findAll();
+    }
+
+    @Override
+    public Item createItem(Item item) {
+        return repository.save(item);
+    }
+
+    @Override
+    public Item updateItem(Item item) {
+        return repository.save(item);
+    }
+
+    @Override
+    public void deleteItem(Long id) {
+        repository.deleteById(id);
+    }
+}
